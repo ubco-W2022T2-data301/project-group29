7679530dddcbec6b2fbbe92e8691e837f78832cd
patrickma6199 <patrickma6199@gmail.com>
2023-03-26 17:47:51 -0700

    Fixed up the processing data method and added some more plots

--- analysis/code/project_functions1.py
+++ analysis/code/project_functions1.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import random
 
 def loadAndProcess(csv):
     """Loads and processes data for this specific analysis question (analysis 1)
@@ -12,7 +13,9 @@ def loadAndProcess(csv):
     
     # My method is written differently from the sample method since I wanted to drop columns before removing NaN values
     
-    rawData = pd.read_csv("../data/raw/mxmh_survey_results.csv")
+    random.seed(68504364)
+    
+    rawData = pd.read_csv(csv)
     modifiedData =\
         rawData.drop(["Timestamp","Age","Exploratory","Foreign languages","BPM","Frequency [Classical]","Frequency [Country]","Frequency [EDM]","Frequency [Folk]","Frequency [Gospel]","Frequency [Hip hop]","Frequency [Jazz]","Frequency [K pop]","Frequency [Latin]","Frequency [Lofi]","Frequency [Metal]","Frequency [Pop]","Frequency [R&B]","Frequency [Rap]","Frequency [Rock]","Frequency [Video game music]","Music effects","Permissions"], axis = 1)\
         .dropna().reset_index(drop = True)\
@@ -21,5 +24,10 @@ def loadAndProcess(csv):
         .rename(columns = {"Hours per day" : "HPD"})\
         .rename(columns = {"While working" : "WW"})\
         .rename(columns = {"Fav genre" : "FG"})
+    # add jitter to the Hours per Day
+    jitteredHPD = pd.Series()
+    for i in range(0,len(modifiedData["HPD"])):
+        jitteredHPD.at[i] = modifiedData["HPD"][i] + (random.random() * 0.2 - 0.1)  # adds a value of [-0.1,0.1)
+    modifiedData = modifiedData.assign(JHPD = jitteredHPD)
     
     return modifiedData
