e3251bb387284da058b4c4d83ba944dc980ebcff
patrickma6199 <patrickma6199@gmail.com>
2023-03-29 15:40:50 -0700

    changed some plot formats, research question, methods for data wrangling, and added some observations

--- analysis/code/project_functions1.py
+++ analysis/code/project_functions1.py
@@ -3,6 +3,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import random
+import scipy.stats as stats
 
 def loadAndProcess(csv):
     """Loads and processes data for this specific analysis question (analysis 1)
@@ -17,13 +18,11 @@ def loadAndProcess(csv):
     
     rawData = pd.read_csv(csv)
     modifiedData =\
-        rawData.drop(["Timestamp","Age","Exploratory","Foreign languages","BPM","Frequency [Classical]","Frequency [Country]","Frequency [EDM]","Frequency [Folk]","Frequency [Gospel]","Frequency [Hip hop]","Frequency [Jazz]","Frequency [K pop]","Frequency [Latin]","Frequency [Lofi]","Frequency [Metal]","Frequency [Pop]","Frequency [R&B]","Frequency [Rap]","Frequency [Rock]","Frequency [Video game music]","Music effects","Permissions"], axis = 1)\
+        rawData.drop(["Primary streaming service","Fav genre","Timestamp","Age","Exploratory","Foreign languages","BPM","Frequency [Classical]","Frequency [Country]","Frequency [EDM]","Frequency [Folk]","Frequency [Gospel]","Frequency [Hip hop]","Frequency [Jazz]","Frequency [K pop]","Frequency [Latin]","Frequency [Lofi]","Frequency [Metal]","Frequency [Pop]","Frequency [R&B]","Frequency [Rap]","Frequency [Rock]","Frequency [Video game music]","Music effects","Permissions"], axis = 1)\
         .dropna().reset_index(drop = True)\
         .assign(total_score = rawData["Anxiety"] + rawData["OCD"] + rawData["Depression"] + rawData["Insomnia"])\
-        .rename(columns = {"Primary streaming service" : "PSS"})\
         .rename(columns = {"Hours per day" : "HPD"})\
         .rename(columns = {"While working" : "WW"})\
-        .rename(columns = {"Fav genre" : "FG"})
     # add jitter to the Hours per Day
     jitteredHPD = pd.Series()
     for i in range(0,len(modifiedData["HPD"])):
@@ -31,3 +30,9 @@ def loadAndProcess(csv):
     modifiedData = modifiedData.assign(JHPD = jitteredHPD)
     
     return modifiedData
+
+def rAndPValue(data, y, x, **kws):
+    r, p = stats.pearsonr(data[y], data[x])
+    ax = plt.gca()
+    ax.text(.05, .8, 'r = {:.4f}, p = {:.4f}'.format(r, p),
+            transform=ax.transAxes)
